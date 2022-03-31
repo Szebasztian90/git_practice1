e90cd7ffe52dc3c36991c38df9ffb19702a349d9
Sebastian Dulau <dulausebastian@gmail.com>
2022-03-31 14:17:54 +0100

    work 3 is done and about to be pushed to Github after commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,4 +4,9 @@ public class HelloWorld {
 
     //Ding awesome work 2!
 
+    // Doing awesome word 3 !
+
+    // Awesome work 3 is done and ready to be committed.
+    // and ready to be pushed to remote repository
+
 }
